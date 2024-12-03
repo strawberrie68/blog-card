9a64d022432a89bf0dea82d391f4875a8f99d41e
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-12-03 14:42:45 -0800

    add: initial commits and assets

---
+++ components/layouts/SimpleLayout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+interface LayoutProps {
+    children: ReactNode;
+    className?: string;
+}
+
+const SimpleLayout = ({ children, className = '' }: LayoutProps) => {
+    return (
+        <div className={`min-h-screen bg-gradient-to-b from-gray-50 to-[#d2d6db] mx-auto pt-[120px] ${className}`}>
+            <main className="container mx-auto flex justify-center">
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default SimpleLayout;
